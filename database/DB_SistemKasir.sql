1da0ef8f61fe0fed9e864ceddbf9b26007e80e8f
ezbim <bhima.pilot@gmail.com>
2024-01-20 10:38:46 +0700

    Database Terbaru 3 Tabel dan sudah diimplementasi relation antar tabel

--- database/DB_SistemKasir.sql
+++ database/DB_SistemKasir.sql
@@ -15,24 +15,44 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for laravel
-CREATE DATABASE IF NOT EXISTS `laravel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `laravel`;
+-- Dumping database structure for sistemkasir
+CREATE DATABASE IF NOT EXISTS `sistemkasir` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `sistemkasir`;
 
--- Dumping structure for table laravel.products
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `price` decimal(8,2) NOT NULL,
-  `description` text DEFAULT NULL,
+-- Dumping structure for table sistemkasir.barang_transaksi
+CREATE TABLE IF NOT EXISTS `barang_transaksi` (
+  `id_barang_transaksi` int(11) NOT NULL AUTO_INCREMENT,
+  `id_transaksi` int(11) DEFAULT NULL,
+  `id_produk` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_barang_transaksi`),
+  KEY `FK_barang_transaksi_transaksi` (`id_transaksi`),
+  KEY `FK_barang_transaksi_produk` (`id_produk`),
+  CONSTRAINT `FK_barang_transaksi_produk` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_barang_transaksi_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table sistemkasir.produk
+CREATE TABLE IF NOT EXISTS `produk` (
+  `id_produk` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_produk` varchar(50) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `jumlah_stok` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_produk`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table sistemkasir.transaksi
+CREATE TABLE IF NOT EXISTS `transaksi` (
+  `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,
+  `total` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `Image` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `products_code_unique` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id_transaksi`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Data exporting was unselected.
 
